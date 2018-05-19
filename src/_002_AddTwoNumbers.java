ce23112326b15a723ff07b0a2a281aad0f6c8c56
kedup <hxemailhx@gmail.com>
2018-05-19 23:25:57 +0800

    commit

--- src/_002_AddTwoNumbers.java
+++ src/_002_AddTwoNumbers.java
@@ -2,6 +2,12 @@ import util.ListNode;
 
 public class _002_AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        int sum=0;
+        ListNode p1 = l1, p2 = l2;
+        while (p1 != null && p2 != null) {
+            
+        }
 
         return l1;
     }
--- src/util/ListNode.java
+++ src/util/ListNode.java
@@ -2,5 +2,9 @@ package util;
 
 public class ListNode {
     int val;
+    ListNode next = null;
 
+    public ListNode(int val) {
+        this.val = val;
+    }
 }
