dc630f734388b4bdccd0f7c61d183146e4b5b342
kedup <hxemailhx@gmail.com>
2018-05-24 16:45:11 +0800

    第一遍

---
+++ src/_009_PalindromeNumber.java
@@ -0,0 +1,23 @@
+/**
+ * @author kedupmail@gmail.com
+ * @date 2018/5/24 15:33
+ */
+public class _009_PalindromeNumber {
+    public static boolean isPalindromeNumber(int num) {
+        if (num < 0 || num == 0) {
+            return false;
+        }
+        int res = 0;
+        int palin = num;
+        while (num > 0) {
+            res = res * 10 + num % 10;
+            num /= 10;
+        }
+        return palin == res;
+    }
+
+    public static void main(String[] args) {
+        int num = 123321;
+        System.out.println(isPalindromeNumber(num));
+    }
+}
